c3d4d390e8de00e3fbdf26bada7bcfa1926082eb
rcastillod <rodrigo.castillo@global3.cl>
2023-01-06 11:35:27 -0300

    feat: :sparkles: Header component completed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,21 @@
 /* ----------- Nav trigger ---------- */
-const navTrigger = document.getElementById('navTrigger')
+const navTriggerOpen = document.getElementById('navTrigger')
+const navTriggerClose = document.getElementById('closeTrigger')
+const mobilePanel = document.getElementById('mobileNav')
+const mobilePanelOverlay = document.querySelector('.overlay')
 
-navTrigger.addEventListener('click', () => {
-  navTrigger.classList.toggle('is-active')
-})
+const activeMenuPanel = () => {
+  navTrigger.classList.add('is-active')
+  mobilePanel.classList.add('is-open')
+  mobilePanelOverlay.classList.add('is-open')
+}
+
+const deactivateMenuPanel = () => {
+  navTrigger.classList.remove('is-active')
+  mobilePanel.classList.remove('is-open')
+  mobilePanelOverlay.classList.remove('is-open')
+}
+
+navTriggerOpen.addEventListener('click', activeMenuPanel)
+navTriggerClose.addEventListener('click', deactivateMenuPanel)
+mobilePanelOverlay.addEventListener('click', deactivateMenuPanel)
