384cfff4667e3b6c2746324cb164b6523b293987
rcastillod <rodrigo.castillo@global3.cl>
2023-02-01 11:06:31 -0300

    build: :construction_worker: Add task manager
    Add gulp task manager to make use of css autoprefixer and css minify

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+	config: {
+		port: 9050
+	},
+	paths: {
+		root: "./",
+		src: {
+			base: ".",
+			css: "./assets/css",
+			js: "./assets/js",
+			img: "./assets/img"
+		},
+		dist: {
+			base: "./dist",
+			css: "./dist/css",
+			js: "./dist/js",
+			img: "./dist/img"
+		},
+		build: {
+			base: "./build",
+			css: "./build/css",
+			js: "./build/js",
+			img: "./build/img"
+		}
+	}
+}
---
+++ gulpfile.js
@@ -0,0 +1,114 @@
+/**
+*   Gulp with TailwindCSS - An CSS Utility framework                                
+*   Author : Manjunath G                                              
+*   URL : manjumjn.com                                                 
+*   Twitter : twitter.com/manju_mjn                                    
+**/
+
+/*
+  Usage:
+  1. npm install //To install all dev dependencies of package
+  2. npm run dev //To start development and server for live preview
+  3. npm run prod //To generate minifed files for live server
+*/
+
+const { src, dest, task, watch, series, parallel } = require('gulp');
+const del = require('del'); //For Cleaning build/dist for fresh export
+const options = require("./config"); //paths and other options from config.js
+const browserSync = require('browser-sync').create();
+
+const sass = require('gulp-sass')(require('sass')); //For Compiling SASS files
+const postcss = require('gulp-postcss'); //For Compiling tailwind utilities with tailwind config
+const autoprefixer = require('autoprefixer'); //For Compiling tailwind utilities with tailwind config
+const concat = require('gulp-concat'); //For Concatinating js,css files
+const cleanCSS = require('gulp-clean-css');//To Minify CSS files
+const purgecss = require('gulp-purgecss');// Remove Unused CSS from Styles
+
+//Note : Webp still not supported in major browsers including forefox
+//const webp = require('gulp-webp'); //For converting images to WebP format
+//const replace = require('gulp-replace'); //For Replacing img formats to webp in html
+const logSymbols = require('log-symbols'); //For Symbolic Console logs :) :P 
+
+//Load Previews on Browser on dev
+function livePreview(done) {
+  browserSync.init({
+    server: {
+      baseDir: options.paths.dist.base
+    },
+    port: options.config.port || 5000
+  });
+  done();
+}
+
+// Triggers Browser reload
+function previewReload(done) {
+  console.log("\n\t" + logSymbols.info, "Reloading Browser Preview.\n");
+  browserSync.reload();
+  done();
+}
+
+//Development Tasks
+function devHTML() {
+  return src(`${options.paths.src.base}/**/*.html`);
+}
+
+function devStyles() {
+  return src(`${options.paths.src.css}/main.css`)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest(options.paths.src.css))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(dest(options.paths.dist.css));
+}
+
+function watchFiles() {
+  watch(`${options.paths.src.base}/**/*.html`, series(devHTML, previewReload));
+  console.log("\n\t" + logSymbols.info, "Watching for Changes..\n");
+}
+
+function devClean() {
+  console.log("\n\t" + logSymbols.info, "Cleaning dist folder for fresh start.\n");
+  return del([options.paths.dist.base]);
+}
+
+//Production Tasks (Optimized Build for Live/Production Sites)
+function prodHTML() {
+  return src(`${options.paths.src.base}/**/*.html`).pipe(dest(options.paths.build.base));
+}
+
+function prodStyles() {
+  return src(`${options.paths.dist.css}/**/*`)
+    .pipe(purgecss({
+      content: ['src/**/*.{html,js}'],
+      defaultExtractor: content => {
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+        return broadMatches.concat(innerMatches)
+      }
+    }))
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(dest(options.paths.build.css));
+}
+
+function prodClean() {
+  console.log("\n\t" + logSymbols.info, "Cleaning build folder for fresh start.\n");
+  return del([options.paths.build.base]);
+}
+
+function buildFinish(done) {
+  console.log("\n\t" + logSymbols.info, `Production build is complete. Files are located at ${options.paths.build.base}\n`);
+  done();
+}
+
+exports.default = series(
+  devClean, // Clean Dist Folder
+  parallel(devStyles, devHTML), //Run All tasks in parallel
+  livePreview, // Live Preview Build
+  watchFiles // Watch for Live Changes
+);
+
+exports.prod = series(
+  prodClean, // Clean Build Folder
+  parallel(prodStyles, prodHTML), //Run All tasks in parallel
+  buildFinish
+);
